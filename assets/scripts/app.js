41a56c737299a0ce34977d472346ef35294cbe03
clint360 <clint360.rebase@gmail.com>
2022-12-09 15:56:45 +0100

    finished the project

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,69 +1,82 @@
-let inputElement = document.querySelector("#weight");
-let doneButtonElement = document.getElementById('done');
-let resetButtonElement = document.querySelector('.reset');
-let selector = document.getElementById("items");
-let display = document.getElementById('showscreen');
+let inputElement = document.querySelector("#input");
+let doneButtonElement = document.getElementById('add');
+let resetButtonElement = document.getElementById('reset');
+let selector = document.querySelector("#selected");
+let display = document.getElementById('clicked');
 let currentWeight = 0;
+let currentValue = 0;
 let statusOfCode;
 let count = 0;
+let j = 0;
 
 function Knapsack() {
+  document.querySelector("#max-w").textContent = inputElement.value
 	if (inputElement.value == "") {
 		inputElement.placeholder = "⚠️ Please Enter a value ";
 		inputElement.style = " animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;"
 	} else {
 		selector.addEventListener("change", () => {
-
+      j++
 			if (currentWeight < inputElement.value) {
 
-				display.innerHTML += selector.value + " ";
-
+				display.innerHTML += j + ". " + selector.value + "<br>" ;
+        selector.value.disabled = true
 				for (let i = 0; i < itemValues.length; i++) {
 
 					if (itemValues[i].name == selector.value) {
-
+            selector.value.disabled = true
 						currentWeight += itemValues[i].weight;
+            currentValue  += itemValues[i].value;
 					}
 				}
 				for (let i = 0; i < itemValues.length; i++) {
 
-					if (itemValues[i].weight + currentWeight > inputElement.value) {
-
-						if (selector[i].value == itemValues[i].name) {
-
+					if (itemValues[i].weight + currentWeight > (inputElement.value - 1)) {
+						if (selector[i].value === itemValues[i].name) {
 							selector[i].disabled = true;
+              document.querySelector("#state").style = "background: red";
 
-							if (selector[i].disabled == true) {
-
+							if (selector[i].disabled === true) {
+  
 								count++;
 							}
-							if (count == itemValues.length) {
-
-								document.querySelector("#showscreen").style.background = "red";
-								document.querySelector("#showscreen").style.color = "#fff";
+							if (count === itemValues.length) {
+								
 							}
 						}
 					}
 				}
-				document.querySelector('#calc').innerHTML = "Current Weight is:" + " " + currentWeight;
+        if(currentWeight >= (inputElement.value)) {
+          document.querySelector("#state").style = "background: red";
+          selector.addEventListener('change', () => {document.querySelector(".items").style = "animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;";})
+      }
+				document.querySelector('#status').innerHTML = "Current Weight:" + " " + currentWeight + "<br>" + "Current Value:" + " " + currentValue + "XAF";
 			}
 		})
 	}
-
-	resetButtonElement.addEventListener("click", () => {
-
-	})
-
-
 }
 
 let itemValues = [
-	{ name: "item 1", weight: 3, value: 300 },
-	{ name: "item 2", weight: 1, value: 100 },
-	{ name: "item 3", weight: 2, value: 400 },
-	{ name: "item 4", weight: 1, value: 500 },
-	{ name: "item 5", weight: 5, value: 700 },
-	{ name: "item 6", weight: 4, value: 800 },
-	{ name: "item 7", weight: 3, value: 500 },
-	{ name: "item 8", weight: 2, value: 250 },
+  { name: "iPhone13", weight: 0.75, value: 500000 },
+  { name: "Laptop", weight: 3, value: 250000 },
+  { name: "HeadPhones", weight: 0.25, value: 2000 },
+  { name: "Yams", weight: 1, value: 1500 },
+  { name: "TeddyBear", weight: 0.5, value: 4000 },
+  { name: "Mango", weight: 0.5, value: 1000 },
+  { name: "iMac", weight: 3, value: 300000 },
+  { name: "Pillow", weight: 0.5, value: 1500 },
+  { name: "Corn", weight: 0.5, value: 500 },
+  { name: "Horn", weight: 0.75, value: 1500 },
+  { name: "Trumpet", weight: 2, value: 13500 },
+  { name: "Shacker", weight: 0.5, value: 2000 },
+  { name: "Mouse", weight: 0.5, value: 2000 },
+  { name: "Keyboard", weight: 1, value: 2500 },
+  { name: "RCHelicopter", weight: 1, value: 10000 },
+  { name: "USB Cable", weight: 0.25, value: 1000 },
+  { name: "BluetoothSpeaker", weight: 1.5, value: 5000 },
+  { name: "GamePads", weight: 1, value: 2000 },
+  { name: "PS4", weight: 3, value: 200000 },
+  { name: "iPad", weight: 2, value: 150000 },
 ]
+
+resetButtonElement.addEventListener("click", () => {window.location.reload()})
